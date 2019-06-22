bcd09ab0b4e04ff594086bf3c89c5b651797b38e
vpuhoff <vpuhoff@live.ru>
2019-06-22 16:58:03 +0300

    Разделение JS кода на модули

---
+++ vue/control-templates.js
@@ -0,0 +1,23 @@
+var VueNumControl = {
+    props: ['readonly', 'emitter', 'ikey', 'getData', 'putData'],
+    template: '<input type="number" :readonly="readonly" :value="value" @input="change($event)" @dblclick.stop="" @pointermove.stop=""/>',
+    data() {
+        return {
+            value: 0,
+        }
+    },
+    methods: {
+        change(e) {
+            this.value = +e.target.value;
+            this.update();
+        },
+        update() {
+            if (this.ikey)
+                this.putData(this.ikey, this.value)
+            this.emitter.trigger('process');
+        }
+    },
+    mounted() {
+        this.value = this.getData(this.ikey);
+    }
+}
---
+++ vue/sockets.js
@@ -0,0 +1,1 @@
+var numSocket = new Rete.Socket('Number value');
